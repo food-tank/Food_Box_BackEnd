fd951c380686369ab497e94776a65033dc608ba7
Woongbin06 <kob05314@gmail.com>
2023-10-31 10:11:16 +0900

    init

---
+++ src/main/java/com/food/foodbox/FoodboxApplication.java
@@ -0,0 +1,13 @@
+package com.food.foodbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/food/foodbox/FoodboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.foodbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class FoodboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
