b3ae11c708b5f74a1c345f85b7571b69466be0e0
Woongbin06 <kob05314@gmail.com>
2023-11-07 11:05:23 +0900

    FEAT :: User Domain

---
+++ src/main/java/com/food/foodbox/domain/user/domain/User.java
@@ -0,0 +1,39 @@
+package com.food.foodbox.domain.user.domain;
+
+import com.food.foodbox.domain.user.domain.type.Authority;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String email;
+
+    private String name;
+
+    private String introduce;
+
+    private String imgUrl;
+
+    @Enumerated(EnumType.STRING)
+    private Authority authority;
+
+    @Builder
+    public User(String email, String name, String introduce, String imgUrl, Authority authority) {
+        this.email = email;
+        this.name = name;
+        this.introduce = introduce;
+        this.imgUrl = imgUrl;
+        this.authority = authority;
+    }
+}
