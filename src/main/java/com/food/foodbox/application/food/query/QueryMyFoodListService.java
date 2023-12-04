073cc25e89163b4c0908b5801b5cacf9009eb0ba
Woongbin06 <kob05314@gmail.com>
2023-12-04 09:56:42 +0900

    FEAT :: 내 식단 조회 시 좋아요 여부

--- src/main/java/com/food/foodbox/application/food/query/QueryMyFoodListService.java
+++ src/main/java/com/food/foodbox/application/food/query/QueryMyFoodListService.java
@@ -3,6 +3,7 @@ package com.food.foodbox.application.food.query;
 import com.food.foodbox.domain.food.domain.Food;
 import com.food.foodbox.domain.user.domain.User;
 import com.food.foodbox.infrastructure.persistence.food.FoodRepository;
+import com.food.foodbox.infrastructure.persistence.like.LikeRepository;
 import com.food.foodbox.presetation.food.dto.response.FoodResponse;
 import com.food.foodbox.shared.aunnotation.QueryService;
 import lombok.RequiredArgsConstructor;
@@ -14,11 +15,19 @@ import java.util.List;
 public class QueryMyFoodListService {
 
     private final FoodRepository foodRepository;
+    private final LikeRepository likeRepository;
 
     public List<FoodResponse> execute(User user) {
         List<Food> foods = foodRepository.findByWriterId(user.getId());
         return foods.stream()
-                .map(food -> FoodResponse.of(food, user, true))
+                .map(food -> FoodResponse.of(food, user, isLiked(user, food)))
                 .toList();
+    }
+
+    private Boolean isLiked(User user, Food food) {
+        if (user == null) {
+            return false;
+        }
+        return likeRepository.existsByUserIdAndFoodId(user.getId(), food.getId());
     }
 }
